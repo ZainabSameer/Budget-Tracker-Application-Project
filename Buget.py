bee138ad617c0fa80df6c9d95d65f6403c414000
Zainab <zainabsameer197@gmail.com>
2025-02-14 01:28:13 +0300

    clean the code

--- Buget.py
+++ Buget.py
@@ -1,7 +1,5 @@
 import csv
 from datetime import datetime
-#ACCOUNTS_CSV = 'accounts.csv'
-
 
 def add_new_entry():
     Title = input("enter title ")
@@ -282,17 +280,6 @@ def Manage():
     
     else:
         print("Invalid choice. Please try again.")   
-#def main_menu ():
-    #print(f"1. Add a New Entry:")
-    #print(f"2. Display Account Balance:")
-    #print(f"3. View All Entries: ")
-    #print(f"4. Search Entries:")
-    #print(f"5. Generate Reports:")
-    #print(f"6. Manage Accounts and Categories: ")
-    #print(f"7. exit")
-
-
-#main_menu ()
 
 while(True):
         choice = input ("Choice opation 1. To Add a New Entry , 2. To Display Account Balance , 3. To View All Entries , 4. To Search for Entries , 5.  To Generate Reports , 6.  To Manage Accounts and Categories 7. exit  " )
